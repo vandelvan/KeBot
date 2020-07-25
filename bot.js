3a5f3009c64c85ae0256297410df8f6b73c01efe
vandelvan <vandel_van@hotmail.com>
2020-07-25 18:47:56 -0500

    THIS IS IT I GUESS kinda

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const TwitchApi = require("node-twitch");
 const client_id = process.env.TWITCH_CLIENT;
-const { TwitchOnlineTracker } = require("twitchonlinetracker");
-const tracker = new TwitchOnlineTracker({
-  client_id: client_id, // used for api requests
-  track: ["kevin1229"], // all the channels you want to track
-  debug: true, // whether to debug to console
+const secret = process.env.TWITCH_SECRET;
+const api = new TwitchApi({
+  client_id: client_id,
+  client_secret: secret,
+  isApp: true, // When this option is enabled, an application token will be automatically acquired from twitch.
 });
-
 
 // Initialize bot by connecting to the server
 client.login(process.env.BOT_TOKEN);
@@ -42,21 +42,25 @@ client.on("guildMemberAdd", (member) => {
   member.roles.add(role).catch((e) => console.log(e));
 });
 
-// Listen to live event, it returns StreamData
-tracker.on("live", (streamData) => {
-  client.channels.cache
-    .get("735201896779743353")
-    .send(
-      "@everyone El Kevin ya está stremeando `" +
-        streamData.title +
-        "`, Caile a verlo! https://twitch.tv/kevin1229"
-    )
-    .catch((e) => console.log(e));
-  const attachment = new Discord.MessageAttachment(streamData.thumbnail_url);
-  client.channels.cache
-    .get("735201896779743353")
-    .send(attachment)
-    .catch((e) => console.log(e));
+api.on("ready", () => {
+  const options = {
+    channels: ["kevin1229"],
+  };
+  //Si kevin esta en vivo:
+  api.getStreams(options, (body, response) => {
+    var streamData = body.data[0];
+    client.channels.cache
+      .get("735201896779743353")
+      .send(
+        "@everyone El Kevin ya está stremeando `" +
+          streamData.title +
+          "`, Caile a verlo! https://twitch.tv/kevin1229"
+      )
+      .catch((e) => console.log(e));
+    const attachment = new Discord.MessageAttachment(streamData.thumbnail_url);
+    client.channels.cache
+      .get("735201896779743353")
+      .send(attachment)
+      .catch((e) => console.log(e));
+  });
 });
-// Make sure you listen for errors
-tracker.on("error", (error) => console.error);
