b2e4950105cd866aedf98f50d48e280dbd73a94d
vandelvan <vandel_van@hotmail.com>
2020-07-25 19:07:52 -0500

    didn't work

--- bot.js
+++ bot.js
@@ -1,13 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const TwitchApi = require("node-twitch");
-const client_id = process.env.TWITCH_CLIENT;
-const secret = process.env.TWITCH_SECRET;
-const api = new TwitchApi({
-  client_id: client_id,
-  client_secret: secret,
-  isApp: true, // When this option is enabled, an application token will be automatically acquired from twitch.
-});
+// const TwitchApi = require("node-twitch");
+// const client_id = process.env.TWITCH_CLIENT;
+// const secret = process.env.TWITCH_SECRET;
 
 // Initialize bot by connecting to the server
 client.login(process.env.BOT_TOKEN);
@@ -41,26 +36,16 @@ client.on("guildMemberAdd", (member) => {
   );
   member.roles.add(role).catch((e) => console.log(e));
 });
-
-api.on("ready", () => {
-  const options = {
-    channels: ["kevin1229"],
-  };
-  //Si kevin esta en vivo:
-  api.getStreams(options, (body, response) => {
-    var streamData = body.data[0];
-    client.channels.cache
-      .get("735201896779743353")
-      .send(
-        "@everyone El Kevin ya está stremeando `" +
-          streamData.title +
-          "`, Caile a verlo! https://twitch.tv/kevin1229"
-      )
-      .catch((e) => console.log(e));
-    const attachment = new Discord.MessageAttachment(streamData.thumbnail_url);
-    client.channels.cache
-      .get("735201896779743353")
-      .send(attachment)
-      .catch((e) => console.log(e));
-  });
-});
+//     client.channels.cache
+//       .get("735201896779743353")
+//       .send(
+//         "@everyone El Kevin ya está stremeando `" +
+//           streamData.title +
+//           "`, Caile a verlo! https://twitch.tv/kevin1229"
+//       )
+//       .catch((e) => console.log(e));
+//     const attachment = new Discord.MessageAttachment(streamData.thumbnail_url);
+//     client.channels.cache
+//       .get("735201896779743353")
+//       .send(attachment)
+//       .catch((e) => console.log(e));
